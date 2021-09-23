71b14dc47ce1a1f1004183d4687f7747051c95e6
Jeff Remillard <jeffrey.remillard@verizonconnect.com>
2021-09-23 12:45:38 -0400

    Initial commit of new solution

---
+++ json-logging-log4net/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace json_logging_log4net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
