c11f3cd5859c22771e808c463bb1d08be99d288c
Jeff Remillard <jeffrey.remillard@verizonconnect.com>
2021-09-23 13:02:49 -0400

    Log4net working for file and console appenders.

--- json-logging-log4net/Program.cs
+++ json-logging-log4net/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,41 @@ using System.Threading.Tasks;
 
 namespace json_logging_log4net
 {
+
     class Program
     {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name);
+
         static void Main(string[] args)
         {
+            log.Info("starting up");
+            log.Debug("debug info");
+            log.Warn("warning");
+            try
+            {
+                a();
+            }
+            catch (Exception e)
+            {
+                log.Error("error", e);
+            }
+            log.Info("stopping");
+            Console.ReadKey();
+        }
+
+        private static void a()
+        {
+            b();
+        }
+
+        private static void b()
+        {
+            c();
+        }
+
+        private static void c()
+        {
+            throw new Exception("exception");
         }
     }
 }
